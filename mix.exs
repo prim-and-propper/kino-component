393329d420fc268b9dca1a73a3060977a5f76fcf
Anthony Smith <420061+anthonator@users.noreply.github.com>
2024-09-09 10:36:26 -0400

    chore: added package description

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule KinoComponent.MixProject do
   def project do
     [
       app: :kino_component,
+      description: "Render Phoenix function components in Livebook",
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
