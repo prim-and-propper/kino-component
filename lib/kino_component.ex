849fd813c58179b4af68eb7012dc0d515531c0b5
Anthony Smith <420061+anthonator@users.noreply.github.com>
2024-09-08 21:37:35 -0400

    feat: support rendering Phoenix function components in LiveBook

---
+++ lib/kino_component.ex
@@ -0,0 +1,42 @@
+defmodule KinoComponent do
+  @moduledoc """
+  A kino for rendering a Phoenix function component.
+
+  ## Example
+
+      import Phoenix.Component, only: [link: 1, sigil_H: 2]
+
+      assigns = %{text: "I'm a link!"}
+
+      ~H\"\"\"
+      <.link><%= @text %></.link>
+      \"\"\"
+      |> KinoComponent.new()
+  """
+
+  use Kino.JS
+
+  asset "main.js" do
+    """
+    export function init(ctx, html) {
+      ctx.root.innerHTML = html;
+    }
+    """
+  end
+
+  @doc """
+  Creates a new kino displaying the given HEEx template.
+  """
+  def new(heex) do
+    Kino.JS.new(__MODULE__, to_html(heex))
+  end
+
+  @doc """
+  Renders an HEEx template and returns the resulting HTML as a string.
+  """
+  def to_html(heex) do
+    heex
+    |> Phoenix.HTML.html_escape()
+    |> Phoenix.HTML.safe_to_string()
+  end
+end
--- test/kino_component_test.exs
+++ test/kino_component_test.exs
@@ -1,3 +1,19 @@
-defmodule Kino.ComponentTest do
-  use ExUnit.Case
+defmodule KinoComponentTest do
+  use ExUnit.Case, async: true
+
+  import Phoenix.Component
+
+  test "to_html/1" do
+    assigns = %{}
+
+    heex = ~H"""
+    <.link href="/">I'm a link!</.link>
+    """
+
+    html = KinoComponent.to_html(heex)
+
+    assert html =~ "<a"
+    assert html =~ "I'm a link!"
+    assert html =~ "</a>"
+  end
 end
