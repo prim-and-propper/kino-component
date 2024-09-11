248dc4a1a9cd85de7ac6e6857b21a6af3a4a6eb7
Anthony Smith <420061+anthonator@users.noreply.github.com>
2024-09-11 09:49:29 -0400

    feat: support client side JS events

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,11 @@
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+
 export function init(ctx, html) {
+    let liveSocket = new LiveSocket("/__kino_component_live__", Socket);
+
+    liveSocket.connect();
+    liveSocket.disconnect();
+
     ctx.root.innerHTML = html;
 }
